66c7515dcef493e4c3e82bbd720927d662950c86
PRIYANKA CHAUDHARI <117078620+1PRIYANKA1@users.noreply.github.com>
2023-10-16 04:10:37 -0900

    Add files via upload

---
+++ Slip29B.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.*;
+class underage extends Exception{
+    public underage (String message){
+        super(message);
+    }
+}
+public class Slip29B
+ {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        try{
+            System.out.println("enter candidates age:");
+            int age=sc.nextInt();
+            check(age);
+            System.out.println("candidate is eligible for voting");
+        }catch(underage e){
+            System.out.println("underage:"+e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println("error"+e.getMessage());
+        }finally{
+            
+        }
+        }
+        static void check(int age)throws underage{
+            if(age<18){
+                throw new underage("candidate is underage and not eligible for voting");
+            }
+        }
+    }
+
